7dc817dab3a743b7c6627ecce7509026529d9523
Saiful Islam Reyad <riyadsaif18@gmail.com>
2024-11-07 12:53:50 +0600

    comments added

--- task2/cse_fest_management/segment/competitive.py
+++ task2/cse_fest_management/segment/competitive.py
@@ -1,14 +1,41 @@
-from interfaces.segment_interface import SegmentInterface
+from task2.cse_fest_management.interfaces.segment_Interface import SegmentInterface
 from models.event import Event
 
 class CompetitiveProgramming(Event, SegmentInterface):
+    """
+    The CompetitiveProgramming class represents a specific segment of the CSE Fest: 
+    the Competitive Programming Contest.
+    It inherits from the Event class to manage general event properties and implements 
+    the SegmentInterface to ensure it has methods for participant registration and event starting.
+    """
+
     def __init__(self, name, description):
+        """
+        Initializes a Competitive Programming event with a name and description.
+        
+        Parameters:
+        - name: The name of the Competitive Programming event.
+        - description: A brief description of the event.
+        
+        Uses the Event class's initializer to set these attributes.
+        """
         super().__init__(name, description)
 
     def register_participant(self, participant):
-        # Register a participant for Competitive Programming
-        self.participants.append(participant)
+        """
+        Registers a participant for the Competitive Programming event.
+        
+        Parameters:
+        - participant: The participant object to be added to the event's participant list.
+        
+        This method appends the participant to the `participants` list inherited from the Event class.
+        """
+        self.participants.append(participant)  # Add participant to the event's participant list
 
     def start_event(self):
-        # Start the Competitive Programming event
-        print("Competitive Programming Contest is now starting!")
+        """
+        Starts the Competitive Programming event.
+        
+        This method prints a message to indicate that the Competitive Programming Contest is starting.
+        """
+        print("Competitive Programming Contest is now starting!")  # Simulate starting the event
--- task2/cse_fest_management/services/notifier.py
+++ task2/cse_fest_management/services/notifier.py
@@ -1,5 +1,19 @@
 from interfaces.notification_interface import NotificationInterface
 
 class Notifier(NotificationInterface):
+    """
+    The Notifier class is responsible for sending notifications to participants.
+    It implements the NotificationInterface to ensure a standardized notify_participant method.
+    """
+
     def notify_participant(self, participant, message):
-        print(f"Notification to {participant.name}: {message}")
+        """
+        Sends a notification to a specified participant.
+        
+        Parameters:
+        - participant: The participant to be notified. It is expected to have a 'name' attribute.
+        - message: The notification message to be sent.
+        
+        This method prints a message to simulate sending a notification.
+        """
+        print(f"Notification to {participant.name}: {message}")  # Simulate sending a notification to the participant
